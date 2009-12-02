837cae7bd50a86f097d765ebbfc0074405d20331
mabe <magnus.bertilsson@gmail.com>
2009-12-02 21:41:40 +0100

    091202: Linq2Sql spike.
    Signed-off-by: mabe <magnus.bertilsson@gmail.com>

--- src/BalloonShop.Mvc/Controllers/HomeController.cs
+++ src/BalloonShop.Mvc/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using BalloonShop.Data;
 using BalloonShop.Infrastructure;
 using BalloonShop.Model;
 
+using System.Linq;
+using BalloonShop.Data.Linq2Sql;
+
 namespace BalloonShop.Mvc.Controllers
 {
     [HandleError]
@@ -10,12 +13,33 @@ namespace BalloonShop.Mvc.Controllers
     {
         public ViewResult Index(int? page)
         {
-            int howManyPages;
-            var balloons = CatalogAccess.GetProductsOnCatalogPromotion(page ?? 1,
-                out howManyPages); 
+            //int howManyPages;
+            //var balloons = CatalogAccess.GetProductsOnCatalogPromotion(page ?? 1,
+            //    out howManyPages);
 
-            return View(new PagedList<Balloon>(page ?? 1, 
+            var context = new BalloonShopDataDataContext();
+
+            var howManyPages = context.Products.Where(x => x.OnCatalogPromotion).Count() / BalloonShopConfiguration.ProductsPerPage;
+            var balloons = context.Products
+                .Where(x => x.OnCatalogPromotion)
+                .Skip(((page ?? 1) - 1) * BalloonShopConfiguration.ProductsPerPage)
+                .Take(BalloonShopConfiguration.ProductsPerPage)
+                .ToList().Select(x => new Balloon
+                {
+                    Description = x.Description,
+                    Image = x.Image2FileName,
+                    Name = x.Name,
+                    OnCatalogPromotion = x.OnCatalogPromotion,
+                    OnDepartmentPromotion = x.OnDepartmentPromotion,
+                    Price = x.Price,
+                    Thumb = x.Image1FileName
+                });
+
+            return View(new PagedList<Balloon>(page ?? 1,
                 BalloonShopConfiguration.ProductsPerPage, howManyPages, balloons));
+
+            //return View(new PagedList<Product>(page ?? 1,
+            //    BalloonShopConfiguration.ProductsPerPage, howManyPages, balloons));
         }
     }
 }
---
+++ src/BalloonShop/Data/Linq2Sql/BalloonShopData.cs
@@ -0,0 +1,6 @@
+namespace BalloonShop
+{
+    partial class BalloonShopDataDataContext
+    {
+    }
+}
