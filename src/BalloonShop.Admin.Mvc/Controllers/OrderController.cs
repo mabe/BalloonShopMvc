17bbd964bc95a298b3ddda79d99a0ca374b1064d
Magnus Bertilsson <magnus.bertilsson@gmail.com>
2012-10-16 22:15:57 +0200

    Administration updates, Reference updates.

--- src/BalloonShop.Admin.Mvc/Controllers/OrderController.cs
+++ src/BalloonShop.Admin.Mvc/Controllers/OrderController.cs
@@ -1,10 +1,12 @@
 ﻿using BalloonShop.Model;
 using NHibernate;
+using NHibernate.Criterion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Order = BalloonShop.Model.Order;
 
 namespace BalloonShop.Admin.Mvc.Controllers
 {
@@ -32,5 +34,25 @@ namespace BalloonShop.Admin.Mvc.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult ShoppingCarts(string action, int days) {
+            var query = _session.QueryOver<ShoppingCart>();
+
+            if (days > 0) {
+                query.Where(Restrictions.On<ShoppingCart>(x => x.DateAdded).IsBetween(DateTime.Today.AddDays(days * -1)).And(DateTime.Today));
+            }
+
+            if (action == "count") {
+                ViewBag.Count = query.Select(Projections.CountDistinct("CartId")).RowCount();
+            }
+
+            if (action == "delete") {
+                _session.CreateQuery("delete from ShoppingCart where cartid IN :ids")
+                    .SetParameterList("ids", query.Select(Projections.Distinct(Projections.Property("CartId"))).List<string>())
+                    .ExecuteUpdate();
+            }
+
+            return View();
+        }
     }
 }
